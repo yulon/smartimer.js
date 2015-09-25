524c1b1d9e949fcb0931e8f1fbab62f7624ff4d3
Yuchen Wang <yulon@me.com>
2015-09-26 07:02:05 +0800

    优化效率，彻底干掉后台 timer。

--- smartimer.js
+++ smartimer.js
@@ -2,13 +2,20 @@ if (document.visibilityState) {
 	var Smartimer = (function(win){
 		var ttIds = {};
 		var ilIds = {};
+		var sleepTts = [];
+		var sleepIls = [];
 
 		function StSetTimeout(func, delay) {
 			var id = win.setTimeout(function(){
 				if (document.visibilityState === "visible") {
 					func();
+					delete ttIds[id];
 				}else{
-					win.clearTimeout()
+					win.clearTimeout(id);
+					sleepTts.push({
+						i: "st" + id,
+						f: func
+					})
 				};
 			}, delay)
 			var stId = "st" + id;
@@ -20,6 +27,13 @@ if (document.visibilityState) {
 			var id = win.setInterval(function(){
 				if (document.visibilityState === "visible") {
 					func();
+				}else{
+					win.clearInterval(id);
+					sleepIls.push({
+						i: "st" + id,
+						f: func,
+						d: delay
+					})
 				};
 			}, delay)
 			var stId = "st" + id;
@@ -34,8 +48,41 @@ if (document.visibilityState) {
 
 		function StClearInterval (id) {
 			win.clearInterval(ilIds[id]);
-			delete ttIds[id];
+			delete ilIds[id];
 		}
+
+		document.addEventListener("visibilitychange", function() {
+			if (document.visibilityState === "visible") {
+				for (var i = 0; i < sleepTts.length; i++) {
+					if (ttIds[sleepTts[i].i]) {
+						sleepTts[i].f();
+						delete ttIds[sleepTts[i].i];
+					};
+				};
+
+				for (var i = 0; i < sleepIls.length; i++) {
+					if (ilIds[sleepIls[i].i]) {
+						var func = sleepIls[i].f;
+						var delay = sleepIls[i].d;
+						ilIds[sleepIls[i].i] = win.setInterval(function(){
+							if (document.visibilityState === "visible") {
+								func();
+							}else{
+								win.clearInterval(id);
+								sleepIls.push({
+									i: "st" + id,
+									f: func,
+									d: delay
+								})
+							};
+						}, delay)
+					};
+				};
+
+				var sleepTts = [];
+				var sleepIls = [];
+			};
+		});
 
 		return {
 			setTimeout: StSetTimeout,
