58565b72dbfedfbcff1f79d3ec76583f4dbaeda7
Yuchen Wang <yulon@me.com>
2015-09-26 02:23:39 +0800

    内建 ID

--- smartimer.js
+++ smartimer.js
@@ -1,24 +1,47 @@
 if (document.visibilityState) {
 	var Smartimer = (function(win){
+		var ttIds = {};
+		var ilIds = {};
+
 		function StSetTimeout(func, delay) {
-			return win.setTimeout(function(){
+			var id = win.setTimeout(function(){
 				if (document.visibilityState === "visible") {
 					func();
+				}else{
+					win.clearTimeout()
 				};
 			}, delay)
+			var stId = "st" + id;
+			ttIds[stId] = id;
+			return stId;
 		};
 
 		function StSetInterval(func, delay) {
-			return win.setInterval(function(){
+			var id = win.setInterval(function(){
 				if (document.visibilityState === "visible") {
 					func();
 				};
 			}, delay)
+			var stId = "st" + id;
+			ilIds[stId] = id;
+			return stId;
 		};
+
+		function StClearTimeout (id) {
+			win.clearTimeout(ttIds[id]);
+			delete ttIds[id];
+		}
+
+		function StClearInterval (id) {
+			win.clearInterval(ilIds[id]);
+			delete ttIds[id];
+		}
 
 		return {
 			setTimeout: StSetTimeout,
 			setInterval: StSetInterval,
+			clearTimeout: StClearTimeout,
+			clearInterval: StClearInterval,
 		};
 	})(window);
 };
